1f1ceb2a9e858c60c398f5b945190763a79bd776
mikewayne <mhoeft1@my.chemeketa.edu>
2015-04-20 17:56:01 -0800

    Update pingpong_spec.rb

--- spec/pingpong_spec.rb
+++ spec/pingpong_spec.rb
@@ -6,7 +6,8 @@ describe('Fixnum#pingpong') do
 #  it("prints out the numbers 1 through the inputted number, incremented up one at a time. For example, a 3 would output the array of 1, 2, 3.") do
 #   expect((3).pingpong()).to(eq([1,2,3]))
 # end
- it("Prints out a list of numbers 1 through the input number, replacing numbers divisible by 3 with ping, replacing numbers divisble by 5 with pong, and number divisible by 3 and 5 with ping-pong.") do
+ it("Prints out a list of numbers 1 through the input number, replacing number/s divisible by 3 with "ping", 
+ replacing number/s divisible by 5 with "pong", and number/s divisible by both 3 and 5 with "ping-pong".") do
    expect((7).pingpong()).to(eq([1,2,"ping",4,"pong","ping",7]))
  end
 #  it("checks if divisible by three and checks if divisible by five") do
